3d17ba623c1fa81359f8bfd0ccf1a1ca730d5d69
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2019-04-08 17:10:31 +0530

    i n i t i a l

--- app/src/main/java/com/mywings/smartcarlock/process/Constants.kt
+++ app/src/main/java/com/mywings/smartcarlock/process/Constants.kt
@@ -3,8 +3,8 @@ package com.mywings.smartcarlock.process
 object Constants {
 
     const val URL = "http://mobile.punetraining.com/MyRestService.svc/"
-    const val LOGIN = "Login"
-    const val REGISTRATION = "Registration"
+    const val LOGIN = "SCLS_Login"
+    const val REGISTRATION = "SCLS_Registration"
     const val GET_STATE = "GetState"
     const val SET_STATE = "UpdateState"
     const val GET_VEHICLE = "GetCar"
